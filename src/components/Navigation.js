d17085180997c856dadfa1fb6b554db92c7ae524
devfromfinland <contact@vietphan.info>
2020-05-10 15:53:25 +0300

    feat: add navigation UI

---
+++ src/components/Navigation.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Navbar, Nav } from 'react-bootstrap'
+
+export default function Navigation () {
+  return (
+    <Navbar className="justify-content-between mb-5">
+      <Navbar.Collapse>
+        <Nav.Link href='#'>Home</Nav.Link>
+        <Nav.Link href='#'>Post a question</Nav.Link>
+      </Navbar.Collapse>
+      
+      <Navbar.Collapse className='justify-content-end'>
+        <Navbar.Text>
+          Signed in as: <strong>Viet{/* {Username} */}</strong> (<a href='#'>sign out</a>)
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
+  )
+}
