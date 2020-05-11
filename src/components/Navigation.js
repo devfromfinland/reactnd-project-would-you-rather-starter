1c18c2ef2974f283d66d7d41bbb9b37ba6e278c6
devfromfinland <contact@vietphan.info>
2020-05-11 18:50:38 +0300

    feat: add React Router to components

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import { Navbar, Nav } from 'react-bootstrap'
+import { NavLink } from 'react-router-dom'
 
-export default function Navigation () {
+export default function Navigation (props) {
+  let { authedUser } = props
   return (
     <Navbar className="justify-content-between mb-5">
       <Navbar.Collapse>
-        <Nav.Link href='#'>Home</Nav.Link>
-        <Nav.Link href='#'>Post a question</Nav.Link>
+        <NavLink to='/' exact className='nav-link' activeClassName='active'>Home</NavLink>
+        <NavLink to='/new' className='nav-link' activeClassName='active'>Post a question</NavLink>
       </Navbar.Collapse>
       
       <Navbar.Collapse className='justify-content-end'>
         <Navbar.Text>
-          Signed in as: <strong>Viet{/* {Username} */}</strong> (<a href="#">sign out</a>)
+          Signed in as: <strong>{authedUser}</strong> (<a href="#">sign out</a>)
         </Navbar.Text>
       </Navbar.Collapse>
     </Navbar>
---
+++ src/components/NewQuestion.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
+
+class NewQuestion extends Component {
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>POST NEW QUESTION</Col>
+        </Row>
+      </Container>
+    )
+  }
+}
+
+export default NewQuestion
